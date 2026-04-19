fc9a0c24e66a7e36db3daf9f80400afb55251414
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-19 20:05:45 +0200

    laod dim default reason

--- sql_scripts/gold/4.dim_default_reason.sql
+++ sql_scripts/gold/4.dim_default_reason.sql
@@ -17,4 +17,15 @@ CREATE TABLE gold.dim_default_reason (
 
     CONSTRAINT pk_dim_default_reason PRIMARY KEY (default_reason_key)
 );
-
+GO
+INSERT INTO gold.dim_default_reason (default_reason, reason_category, is_preventable)
+VALUES
+    ('Job Loss',             'Economic',  1),
+    ('Business Failure',     'Economic',  1),
+    ('Medical Emergency',    'Personal',  0),
+    ('Fraud',                'Fraud',     1),
+    ('Divorce',              'Personal',  0),
+    ('Death',                'Personal',  0),
+    ('Overcommitment',       'Personal',  1),
+    ('Economic Downturn',    'Economic',  0),
+    ('Unknown',              'Unknown',   0);
